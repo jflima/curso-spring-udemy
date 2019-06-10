ca1086d1610494b897fcd5224ff540832b1e6985
Jamerson Lima <jamersonfpl@gmail.com>
2019-06-09 23:39:01 -0300

    Adicionando entidades Cliente, TipoCliente, Endereco e Telefone

--- src/main/java/dev/jamerson/cursomc/CursomcApplication.java
+++ src/main/java/dev/jamerson/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import dev.jamerson.cursomc.domain.Categoria;
 import dev.jamerson.cursomc.domain.Cidade;
+import dev.jamerson.cursomc.domain.Cliente;
+import dev.jamerson.cursomc.domain.Endereco;
 import dev.jamerson.cursomc.domain.Estado;
 import dev.jamerson.cursomc.domain.Produto;
+import dev.jamerson.cursomc.domain.enums.TipoCliente;
 import dev.jamerson.cursomc.repositories.CategoriaRepository;
 import dev.jamerson.cursomc.repositories.CidadeRepository;
+import dev.jamerson.cursomc.repositories.ClienteRepository;
+import dev.jamerson.cursomc.repositories.EnderecoRepository;
 import dev.jamerson.cursomc.repositories.EstadoRepository;
 import dev.jamerson.cursomc.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -65,6 +76,17 @@ public class CursomcApplication implements CommandLineRunner {
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "986856116"));
+		
+		Endereco end1 = new Endereco(null, "Rua Flores", "300", "Ap 303", "Jardim", "38220034", cli1, c1);
+		Endereco end2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38770012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(end1, end2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(end1, end2));
 	}
 
 	
---
+++ src/main/java/dev/jamerson/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package dev.jamerson.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import dev.jamerson.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
